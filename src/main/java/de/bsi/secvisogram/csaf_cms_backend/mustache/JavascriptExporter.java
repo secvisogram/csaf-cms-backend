29d3330845f826ac0ecccf09a83667ff6014f65b
wofl <3864493+wofl@users.noreply.github.com>
2022-07-12 15:38:20 +0200

    refactor(export): add finals and some documentation to the JavascriptExporter
    #34

--- src/main/java/de/bsi/secvisogram/csaf_cms_backend/mustache/JavascriptExporter.java
+++ src/main/java/de/bsi/secvisogram/csaf_cms_backend/mustache/JavascriptExporter.java
@@ -19,8 +19,14 @@ import javax.annotation.Nonnull;
  */
 public class JavascriptExporter {
 
-
-    public String createHtml(String jsonString) throws IOException {
+    /**
+     * Create an HTML export from the provided advisory document (as JSON)
+     *
+     * @param advisoryJson the advisory that should be exported (in JSON format)
+     * @return the HTML document as a String
+     * @throws IOException on any error regarding disk write/read
+     */
+    public String createHtml(@Nonnull final String advisoryJson) throws IOException {
 
         // DocumentEntity.mjs is imported from Script.mjs
         // It has to bbe available in the Context WorkingDirectory
@@ -28,19 +34,19 @@ public class JavascriptExporter {
         final Path tempDir = Files.createTempDirectory("mustache");
         final Path tempDocFile = tempDir.resolve(documentEntityScript);
 
-        try (InputStream in = JavascriptExporter.class.getResourceAsStream(documentEntityScript)) {
+        try (final InputStream in = JavascriptExporter.class.getResourceAsStream(documentEntityScript)) {
             Files.write(tempDocFile, in.readAllBytes());
         }
-        var jsContext = Context.newBuilder("js")
+        final var jsContext = Context.newBuilder("js")
                 .allowExperimentalOptions(true)
                 .allowIO(true)
                 .option("js.esm-eval-returns-exports", "true")
                 .currentWorkingDirectory(tempDir)
                 .build();
 
-        try (InputStream templateResource = JavascriptExporter.class.getResourceAsStream("Template.html");
-             InputStream mustacheResource = JavascriptExporter.class.getResourceAsStream("mustache.min.js");
-             InputStream scriptResource = JavascriptExporter.class.getResourceAsStream("Script.mjs")) {
+        try (final InputStream templateResource = JavascriptExporter.class.getResourceAsStream("Template.html");
+             final InputStream mustacheResource = JavascriptExporter.class.getResourceAsStream("mustache.min.js");
+             final InputStream scriptResource = JavascriptExporter.class.getResourceAsStream("Script.mjs")) {
             final var template = new String(templateResource.readAllBytes(), StandardCharsets.UTF_8);
             final var mustacheSource = Source.newBuilder("js", this.createResourceReader(mustacheResource), "mustache.js")
                     .build();
@@ -50,7 +56,7 @@ public class JavascriptExporter {
                     .build();
             final Value scriptResult = jsContext.eval(scriptSource);
             final Value renderFunction = scriptResult.getMember("renderWithMustache");
-            final Object result = renderFunction.execute(template, jsonString);
+            final Object result = renderFunction.execute(template, advisoryJson);
             return result.toString();
         }
     }
