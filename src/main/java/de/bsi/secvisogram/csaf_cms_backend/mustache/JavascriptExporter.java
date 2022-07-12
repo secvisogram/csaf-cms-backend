734210fcaf6cd17c8e97a061e9733032d28eb0b3
wofl <3864493+wofl@users.noreply.github.com>
2022-07-12 15:38:19 +0200

    refactor(export): refactor JavascriptExporter for better readability
    #34

--- src/main/java/de/bsi/secvisogram/csaf_cms_backend/mustache/JavascriptExporter.java
+++ src/main/java/de/bsi/secvisogram/csaf_cms_backend/mustache/JavascriptExporter.java
@@ -3,12 +3,16 @@ package de.bsi.secvisogram.csaf_cms_backend.mustache;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
 import org.graalvm.polyglot.Context;
 import org.graalvm.polyglot.Source;
 import org.graalvm.polyglot.Value;
+
+import javax.annotation.Nonnull;
 
 /**
  * Create Html String from a mustache template file and Json Input File
@@ -21,34 +25,37 @@ public class JavascriptExporter {
         // DocumentEntity.mjs is imported from Script.mjs
         // It has to bbe available in the Context WorkingDirectory
         final String documentEntityScript = "DocumentEntity.mjs";
-        Path tempDir = Files.createTempDirectory("mustache");
-        Path tempDocFile = tempDir.resolve(documentEntityScript);
+        final Path tempDir = Files.createTempDirectory("mustache");
+        final Path tempDocFile = tempDir.resolve(documentEntityScript);
 
         try (InputStream in = JavascriptExporter.class.getResourceAsStream(documentEntityScript)) {
             Files.write(tempDocFile, in.readAllBytes());
         }
-        var ctx = Context.newBuilder("js")
+        var jsContext = Context.newBuilder("js")
                 .allowExperimentalOptions(true)
                 .allowIO(true)
                 .option("js.esm-eval-returns-exports", "true")
                 .currentWorkingDirectory(tempDir)
                 .build();
 
-        String template = "";
-        try (InputStream in = JavascriptExporter.class.getResourceAsStream("Template.html")) {
-            template = new String(in.readAllBytes(), StandardCharsets.UTF_8);
-        }
-
-        try (InputStream in = JavascriptExporter.class.getResourceAsStream("Script.mjs");
-             InputStream mustache = JavascriptExporter.class.getResourceAsStream("mustache.min.js")) {
-
-            ctx.eval(Source.newBuilder("js", new InputStreamReader(mustache, StandardCharsets.UTF_8), "mustache.js").build());
-            Value scipts = ctx.eval(Source.newBuilder("js", new InputStreamReader(in, StandardCharsets.UTF_8), "Script.mjs").mimeType("application/javascript+module").build());
-
-            Value member = scipts.getMember("renderWithMustache");
-
-            Object result = member.execute(template, jsonString);
+        try (InputStream templateResource = JavascriptExporter.class.getResourceAsStream("Template.html");
+             InputStream mustacheResource = JavascriptExporter.class.getResourceAsStream("mustache.min.js");
+             InputStream scriptResource = JavascriptExporter.class.getResourceAsStream("Script.mjs")) {
+            final var template = new String(templateResource.readAllBytes(), StandardCharsets.UTF_8);
+            final var mustacheSource = Source.newBuilder("js", this.createResourceReader(mustacheResource), "mustache.js")
+                    .build();
+            jsContext.eval(mustacheSource);
+            final var scriptSource = Source.newBuilder("js", createResourceReader(scriptResource), "Script.mjs")
+                    .mimeType("application/javascript+module")
+                    .build();
+            final Value scriptResult = jsContext.eval(scriptSource);
+            final Value renderFunction = scriptResult.getMember("renderWithMustache");
+            final Object result = renderFunction.execute(template, jsonString);
             return result.toString();
         }
+    }
+
+    private Reader createResourceReader(@Nonnull final InputStream input) {
+        return new InputStreamReader(input, StandardCharsets.UTF_8);
     }
 }
