087e0a31362a5603582845cf02d2e6e218666696
mfd2007 <58845044+mfd2007@users.noreply.github.com>
2023-07-26 08:36:12 +0200

    Use lambda expression to config CSRF.

--- src/main/java/de/bsi/secvisogram/csaf_cms_backend/config/SecurityConfig.java
+++ src/main/java/de/bsi/secvisogram/csaf_cms_backend/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
@@ -26,20 +27,18 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain configure(HttpSecurity http) throws Exception {
         http
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .oauth2ResourceServer((oauth2ResourceServer) ->
-                  oauth2ResourceServer.jwt((jwt) ->
-                    jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
-                  )
-                );
-                
+          .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+          .oauth2ResourceServer((oauth2ResourceServer) ->
+            oauth2ResourceServer.jwt((jwt) ->
+              jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())
+            )
+        );
         if (this.isCsrfEnabled) {
-            http
-                .csrf(csrf ->
-                    csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-                );
+          http.csrf(csrf ->
+            csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+          );
         } else {
-            http.csrf(csrf -> csrf.disable());
+          http.csrf(AbstractHttpConfigurer::disable);
         }
         
         return http.build();
