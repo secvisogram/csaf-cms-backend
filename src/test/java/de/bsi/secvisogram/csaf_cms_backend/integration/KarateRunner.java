66749fce629f284e60768e43414102c4ed83ea6f
mfd2007 <58845044+mfd2007@users.noreply.github.com>
2023-08-08 14:43:18 +0200

    Add test case for local integration tests.

--- src/test/java/de/bsi/secvisogram/csaf_cms_backend/integration/KarateRunner.java
+++ src/test/java/de/bsi/secvisogram/csaf_cms_backend/integration/KarateRunner.java
@@ -5,12 +5,17 @@ import com.intuit.karate.junit5.Karate;
 
 public class KarateRunner {
   @Karate.Test
-  Karate testSample() {
-    
+  Karate testExport() { 
     String[] testCases = new String[] { 
-      //"classpath:de/bsi/secvisogram/csaf_cms_backend/integration/sample.feature",
+      "classpath:de/bsi/secvisogram/csaf_cms_backend/integration/export.feature"
+    };
+    return Karate.run(testCases);
+  }
+  
+  @Karate.Test
+  Karate testFullWorkflow() { 
+    String[] testCases = new String[] { 
       "classpath:de/bsi/secvisogram/csaf_cms_backend/integration/fullworkflow.feature"
-      //  "classpath:de/bsi/secvisogram/csaf_cms_backend/integration/export.feature"
     };
     return Karate.run(testCases);
   }
