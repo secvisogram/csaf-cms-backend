7469094d5322c9654a4fa2a8a1c49de1f9d770cc
mfd2007 <58845044+mfd2007@users.noreply.github.com>
2024-08-21 11:49:51 +0200

    test: Update test to match changed template

--- src/test/java/de/bsi/secvisogram/csaf_cms_backend/mustache/JavascriptExporterNoLogoTest.java
+++ src/test/java/de/bsi/secvisogram/csaf_cms_backend/mustache/JavascriptExporterNoLogoTest.java
@@ -67,12 +67,10 @@ class JavascriptExporterNoLogoTest {
 
     private static final String resultHtmlNoLogo =
             """
-                    <!DOCTYPE html>
-                    <html lang="en">
+<!DOCTYPE html>
+<html lang="en">
 
                     <head>
-  <!-- link rel="stylesheet" href="https://unpkg.com/gutenberg-css" charset="utf-8"> -->
-  <!-- link rel="stylesheet" href="https://unpkg.com/gutenberg-css/dist/themes/modern.min.css" charset="utf-8"> -->
   <meta charset="utf-8" />
   <style>
 		<!--
@@ -493,84 +491,89 @@ class JavascriptExporterNoLogoTest {
   </style>
 </head>
 
-                    <body>
-                    
-                     <h1>exxcellent-2021AB123: TestRSc</h1>
-
-                      <table>
-                        <tr>
-                          <td>Publisher: exccellent</td>
-                          <td>Document category: generic_csaf</td>
-                        </tr>
-                        <tr>
-                          <td>Initial release date: 2022-01-12T11:00:00.000Z</td>
-                          <td>Engine: Secvisogram 1.10.0</td>
-                        </tr>
-                        <tr>
-                          <td>Current release date: 2022-01-11T11:00:00.000Z</td>
-                          <td>Build Date: 2022-01-11T04:07:27.246Z</td>
-                        </tr>
-                        <tr>
-                          <td>Current version: 0.0.1</td>
-                          <td>Status: draft</td>
-                        </tr>
-                        <tr>
-                          <td>CVSSv3.1 Base Score: 0</td>
-                          <td>Severity:
-                            
-                             
-                          </td>
-                        </tr>
-                        <tr>
-                          <td>Original language: </td>
-                          <td>Language: </td>
-                        </tr>
-                        <tr>
-                          <td colspan="2">Also referred to: </td>
-                        </tr>
-                      </table>
+<body>
 
 
+  <h1>exxcellent-2021AB123: TestRSc</h1>
 
-                      <h2>Vulnerabilities</h2>
-
-                      <h2>Acknowledgments</h2>
-                      exccellent thanks the following parties for their efforts:
-                      <ul>
-
-                          <li>Rainer, Gregor, Timo from exxcellent contribute  for Summary 1234 (see: <a href=https:&#x2F;&#x2F;exccellent.de>https:&#x2F;&#x2F;exccellent.de</a>, <a href=https:&#x2F;heise.de>https:&#x2F;heise.de</a>)</li> 
-                      </ul>
-
-
-                        <h2>exccellent</h2>
-                        <p>Namespace: https:&#x2F;&#x2F;exccellent.de</p>
-                        <p></p>
-                        <p></p>
-
-
-                      <h2>Revision history</h2>
-                      <table>
-                        <thead>
-                          <tr>
-                            <th>Version</th>
-                            <th>Date of the revision</th>
-                            <th>Summary of the revision</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                              <td>0.0.1</td>
-                              <td>2022-01-12T11:00:00.000Z</td>
-                              <td>Test rsvSummary</td>
-                            </tr>
-                        </tbody>
-                      </table>
+  <table>
+    <tr>
+      <td>Publisher: exccellent</td>
+      <td>Document category: generic_csaf</td>
+    </tr>
+    <tr>
+      <td>Initial release date: 2022-01-12T11:00:00.000Z</td>
+      <td>Engine: Secvisogram 1.10.0</td>
+    </tr>
+    <tr>
+      <td>Current release date: 2022-01-11T11:00:00.000Z</td>
+      <td>Build Date: 2022-01-11T04:07:27.246Z</td>
+    </tr>
+    <tr>
+      <td>Current version: 0.0.1</td>
+      <td>Status: draft</td>
+    </tr>
+    <tr>
+      <td>CVSSv3.1 Base Score: 0</td>
+      <td>Severity:
+        
+         
+      </td>
+    </tr>
+    <tr>
+      <td>Original language: </td>
+      <td>Language: </td>
+    </tr>
+    <tr>
+      <td colspan="2">Also referred to: </td>
+    </tr>
+  </table>
 
 
 
-                    </body>
+  <h2>Vulnerabilities</h2>
 
-                    </html>
+  <h2>Acknowledgments</h2>
+  exccellent thanks the following parties for their efforts:
+  <ul>
+
+      <li>Rainer, Gregor, Timo from exxcellent contribute  for Summary 1234 (see: 
+  <a >https:&#x2F;&#x2F;exccellent.de</a>
+, 
+  <a >https:&#x2F;heise.de</a>
+)</li>
+  </ul>
+
+
+    <h2>exccellent</h2>
+    <p>Namespace: https:&#x2F;&#x2F;exccellent.de</p>
+    <p></p>
+    <p></p>
+
+
+  <h2>Revision history</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>Version</th>
+        <th>Date of the revision</th>
+        <th>Summary of the revision</th>
+      </tr>
+    </thead>
+    <tbody>
+        <tr>
+          <td>0.0.1</td>
+          <td>2022-01-12T11:00:00.000Z</td>
+          <td>Test rsvSummary</td>
+        </tr>
+    </tbody>
+  </table>
+
+
+
+</body>
+
+</html>
                     """;
 
     @Autowired
