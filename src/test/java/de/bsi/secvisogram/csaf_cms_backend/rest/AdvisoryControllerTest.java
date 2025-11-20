e34c4cf8d036137f1021bf10c5df6fb9260d873d
mfd2007 <58845044+mfd2007@users.noreply.github.com>
2025-11-20 17:27:55 +0100

    chore: change deprecated feature, remove unused imports

--- src/test/java/de/bsi/secvisogram/csaf_cms_backend/rest/AdvisoryControllerTest.java
+++ src/test/java/de/bsi/secvisogram/csaf_cms_backend/rest/AdvisoryControllerTest.java
@@ -52,11 +52,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest(AdvisoryController.class)
@@ -66,10 +66,10 @@ public class AdvisoryControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private AdvisoryService advisoryService;
 
-    @MockBean
+    @MockitoBean
     private DocumentTemplateService templateService;
 
     @Autowired
